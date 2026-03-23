341568a96cc5e3758263399ff46ceb7b8eda75e4
HanskiZensei <hannu.vainio2@metropolia.fi>
2026-03-23 10:51:27 +0200

    add assignment1

---
+++ module_10/assignment1.py
@@ -0,0 +1,28 @@
+class Elevator:
+    def __init__(self, bottom_floor, top_floor):
+        self.current_floor = bottom_floor
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+
+
+    def go_to_floor(self,floor):
+        if floor < self.bottom_floor or floor > self.top_floor:
+            return
+
+        if self.current_floor < floor:
+            for h in range(floor-self.current_floor):
+                self.floor_up()
+                print(f"Current floor: {self.current_floor}")
+        elif self.current_floor > floor:
+            for h in range(self.current_floor-floor):
+                self.floor_down()
+                print(f"Current floor: {self.current_floor}")
+        else: print(f"Current floor: {self.current_floor}")
+        return
+
+
+    def floor_up(self):
+        self.current_floor +=1
+
+    def floor_down(self):
+        self.current_floor -=1
